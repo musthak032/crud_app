c8f1164d691c767cdf0b44cee5205ee3d4b8d0a4
musthak032 <musthak0032@gmail.com>
2023-06-12 12:18:54 +0530

    responsive design

--- src/EmployeeCard.js
+++ src/EmployeeCard.js
@@ -1,26 +1,22 @@
-import React from "react";
-
+import React from 'react';
+// import "./EmployeeCard.css"; 
 const EmployeeCard = ({ empdata, LoadDetail, LoadEdit, Removefunction }) => {
   return (
     <div className="card employee-card">
-      <div className="card-content">
-        <div className="card-header">
-          <h5 className="card-title">{empdata.name}</h5>
+      <div className="card-body">
+        <h5 className="card-title">{empdata.name}</h5>
+        <div className="employee-details">
+          <p className="card-text">
+            <strong>Employee ID:</strong> {empdata.id}
+          </p>
+          <p className="card-text">
+            <strong>Email:</strong> {empdata.email}
+          </p>
+          <p className="card-text">
+            <strong>Phone:</strong> {empdata.phone}
+          </p>
         </div>
-        <div className="card-body">
-          <div className="employee-details">
-            <p className="employee-info" style={{ textAlign: "left" }}>
-              <strong>Employee ID:</strong> {empdata.id}
-            </p>
-            <p className="employee-info" style={{ textAlign: "left" }}>
-              <strong>Email:</strong> {empdata.email}
-            </p>
-            <p className="employee-info" style={{ textAlign: "left" }}>
-              <strong>Phone:</strong> {empdata.phone}
-            </p>
-          </div>
-        </div>
-        <div className="card-footer">
+        <div className="d-grid gap-2">
           <button onClick={() => LoadDetail(empdata.id)} className="btn btn-primary">
             View Profile
           </button>
--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import EmployeeCard from "./EmployeeCard";
 import AllEmployeeDataCard from "./AllEmployeeDataCard";
-// import "./EmployeeCard.css";
 import "./Home.css";
 
 const Home = () => {
@@ -16,10 +15,9 @@ const Home = () => {
   const [alldata, setalldata] = useState(null);
 
   const LoadDetail = (id) => {
-
-   
     navigate("/crud_app/" + id);
   };
+
   const handlepageclick = async (data) => {
     console.log("clicked " + data.selected);
     let currentpage = data.selected + 1;
@@ -29,8 +27,6 @@ const Home = () => {
 
   const Removefunction = (id) => {
     if (window.confirm("Do you want to remove")) {
-      // fetch("http://localhost:3001/employee/"+id, {
-
       fetch("https://employee-data-bat9.onrender.com/employee/" + id, {
         method: "DELETE",
       })
@@ -46,17 +42,14 @@ const Home = () => {
         });
     }
   };
+
   const LoadEdit = (id) => {
     navigate("/crud_app/edit/" + id);
   };
 
   useEffect(() => {
-    //fetch("http://localhost:3001/employee")
-
     fetch("https://employee-data-bat9.onrender.com/employee")
-      .then((res) => {
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((resp) => {
         setalldata(resp);
         settotaluser(resp);
@@ -65,7 +58,6 @@ const Home = () => {
         console.log("total page count " + total);
         console.log("total object " + total);
         setpageCount(total);
-        // Use the `total` value as needed
       })
       .catch((err) => {
         console.log(err.message);
@@ -73,12 +65,8 @@ const Home = () => {
   }, []);
 
   useEffect(() => {
-    //fetch("http://localhost:3001/employee")
-
     fetch("https://employee-data-bat9.onrender.com/employee?_page=1&_limit=8")
-      .then((res) => {
-        return res.json();
-      })
+      .then((res) => res.json())
       .then((resp) => {
         setempdata(resp);
       })
@@ -95,87 +83,81 @@ const Home = () => {
     return resp;
   };
 
-  console.log("search value" + search.length);
-
   return (
     <div>
       <div className="card-footer">
         <input
           type="text"
           placeholder="Search..."
-          className="search"
+          className="search form-control"
           onChange={(e) => {
             setsearch(e.target.value);
           }}
-        ></input>
-
-      
-
-        <button className="bttn" type="submit">
-          <Link className=" bttn-text" to="/crud_app/create">
+        />
+        <button className="bttn btn btn-primary" type="submit">
+          <Link className="bttn-text" to="/crud_app/create">
             Add New (+)
           </Link>
         </button>
       </div>
 
-          <h5 style={{ textAlign: "right" }}>Total user {totaluser.length}</h5>
-      <div>
-        <div></div>
-        <div>
-          {search.length !== 0
-            ? alldata &&
-              alldata
-                .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
-                .map((item) => (
-                  <div key={item.id}>
-                    <AllEmployeeDataCard
-                      alldata={item}
-                      LoadEdit={LoadEdit}
-                      Removefunction={Removefunction}
-                      LoadDetail={LoadDetail}
-                    />
-                  </div>
-                ))
-            : empdata &&
-              empdata
-                .filter((item) => item.name.toLowerCase().includes(search))
-                .map((item) => (
-                  <div key={item.id}>
-                    <EmployeeCard
-                      empdata={item}
-                      LoadEdit={LoadEdit}
-                      Removefunction={Removefunction}
-                      LoadDetail={LoadDetail}
-                    />
-                  </div>
-                ))}
+      <h5 className="text-right">Total user {totaluser.length}</h5>
 
-          {search.length === 0 && (
-            <div className="pagination-container">
-              <ReactPaginate
-                previousLabel={"Previous"}
-                nextLabel={"Next"}
-                breakLabel={"..."}
-                pageCount={pageCount}
-                marginPagesDisplayed={2}
-                pageRangeDisplayed={2}
-                onPageChange={handlepageclick}
-                containerClassName={"pagination"}
-                pageClassName={"page-item"}
-                pageLinkClassName={"page-link"}
-                previousClassName={"page-item"}
-                previousLinkClassName={"page-link"}
-                nextClassName={"page-item"}
-                nextLinkClassName={"page-link"}
-                breakClassName={"page-item"}
-                breakLinkClassName={"page-link"}
-                activeClassName={"active"}
-              />
-            </div>
-          )}
-        </div>
+      <div>
+        {search.length !== 0
+          ? alldata &&
+            alldata
+              .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()))
+              .map((item) => (
+                <div key={item.id}>
+                  <AllEmployeeDataCard
+                    alldata={item}
+                    LoadEdit={LoadEdit}
+                    Removefunction={Removefunction}
+                    LoadDetail={LoadDetail}
+                  />
+                </div>
+              ))
+          : empdata &&
+            empdata
+              .filter((item) => item.name.toLowerCase().includes(search))
+              .map((item) => (
+                <div key={item.id}>
+                  <EmployeeCard
+                    empdata={item}
+                    LoadEdit={LoadEdit}
+                    Removefunction={Removefunction}
+                    LoadDetail={LoadDetail}
+                  />
+                </div>
+              ))}
+                {search.length === 0 && (
+                  <div className="pagination-container">
+                    <ReactPaginate
+                      previousLabel={"Previous"}
+                      nextLabel={"Next"}
+                      breakLabel={".."}
+                      pageCount={pageCount}
+                      marginPagesDisplayed={0}
+                      pageRangeDisplayed={2}
+                      onPageChange={handlepageclick}
+                      containerClassName={"pagination"}
+                      pageClassName={"page-item"}
+                      pageLinkClassName={"page-link"}
+                      previousClassName={"page-item"}
+                      previousLinkClassName={"page-link"}
+                      nextClassName={"page-item"}
+                      nextLinkClassName={"page-link"}
+                      breakClassName={"page-item"}
+                      breakLinkClassName={"page-link"}
+                      activeClassName={"active"}
+                    />
+                  </div>
+                )}
+
       </div>
     </div>
   );
 };
+
 export default Home;
